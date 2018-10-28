ce14cbef87a964b1fb2d949923d68ca3230efdde
Jelle Tigchelaar <RespawnDespair@users.noreply.github.com>
2018-10-28 13:50:07 +0100

    less complex

--- stages/02-Kernel/07-run.sh
+++ stages/02-Kernel/07-run.sh
@@ -4,9 +4,9 @@ pushd ${STAGE_WORK_DIR}
 log "Copy the DTBs"
 
 MNT_DIR="${STAGE_WORK_DIR}/mnt"
-sudo cp "${STAGE_WORK_DIR}/linux/arch/arm/boot/dts/*.dtb" "${MNT_DIR}/boot/"
-sudo cp "${STAGE_WORK_DIR}/linux/arch/arm/boot/dts/overlays/*.dtb*" "${MNT_DIR}/boot/overlays/"
-sudo cp "${STAGE_WORK_DIR}/linux/arch/arm/boot/dts/overlays/README" "${MNT_DIR}/boot/overlays/"
+sudo cp "linux/arch/arm/boot/dts/*.dtb" "${MNT_DIR}/boot/"
+sudo cp "linux/arch/arm/boot/dts/overlays/*.dtb*" "${MNT_DIR}/boot/overlays/"
+sudo cp "linux/arch/arm/boot/dts/overlays/README" "${MNT_DIR}/boot/overlays/"
 
 #return
 popd
