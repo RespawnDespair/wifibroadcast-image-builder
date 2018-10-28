37e8e8dcdbb9b4eff92e03e5cbc5b7380035ac6b
Jelle Tigchelaar <RespawnDespair@users.noreply.github.com>
2018-10-28 11:07:07 +0100

    wrong config name

--- stages/02-Kernel/02-run.sh
+++ stages/02-Kernel/02-run.sh
@@ -5,7 +5,7 @@ log "Compile kernel For Pi 1, Pi Zero, Pi Zero W, or Compute Module"
 pushd linux
 
 log "Copy Kernel config"
-cp "${STAGE_DIR}/FILES/.config_db_v6_kernel_4_14_66" ./.conf
+cp "${STAGE_DIR}/FILES/.config_db_v6_kernel_4_14_66" ./.config
 
 #KERNEL=kernel ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make bcmrpi_defconfig
 KERNEL=kernel ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make 
