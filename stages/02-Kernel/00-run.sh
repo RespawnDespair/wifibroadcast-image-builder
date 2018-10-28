2bebea378625fd46a35646a71ebafc1e7c86ad9e
Jelle Tigchelaar <RespawnDespair@users.noreply.github.com>
2018-10-28 10:48:31 +0100

    Download the latest rtl8112au drivers

--- stages/02-Kernel/00-run.sh
+++ stages/02-Kernel/00-run.sh
@@ -7,5 +7,8 @@ rm -r linux
 log "Download the Raspberry Pi Kernel"
 git clone --depth=1 https://github.com/raspberrypi/linux
 
+log "Download the rtl8812au drivers"
+git clone https://github.com/aircrack-ng/rtl8812au.git
+
 #return 
 popd
