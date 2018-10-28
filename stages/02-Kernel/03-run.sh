d3eef68369c0700a069408a0ca1561d86482704e
Jelle Tigchelaar <RespawnDespair@users.noreply.github.com>
2018-10-28 10:35:45 +0100

    Copy conf

--- stages/02-Kernel/03-run.sh
+++ stages/02-Kernel/03-run.sh
@@ -4,7 +4,11 @@ pushd ${STAGE_WORK_DIR}
 log "Compile kernel For Pi 2, Pi 3, Pi 3+, or Compute Module 3"
 pushd linux
 
-KERNEL=kernel7 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make bcm2709_defconfig
+log "Copy Kernel config"
+cp "${STAGE_DIR}/FILES/.config_db_v7_kernel_4_14_66" ./.conf
+
+#KERNEL=kernel7 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make bcm2709_defconfig
+KERNEL=kernel7 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make
 KERNEL=kernel7 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make -j4 zImage modules dtbs
 
 log "Saving kernel as ${STAGE_WORK_DIR}/kernel7.img"
