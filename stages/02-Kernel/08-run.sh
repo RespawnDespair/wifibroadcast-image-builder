0d3b47ce4ab6652c1cb8dc408afb446d58838e46
Jelle Tigchelaar <RespawnDespair@users.noreply.github.com>
2018-10-28 15:08:36 +0100

    no quotes

--- stages/02-Kernel/08-run.sh
+++ stages/02-Kernel/08-run.sh
@@ -4,7 +4,7 @@ pushd ${STAGE_WORK_DIR}
 log "Install QEMU"
 
 MNT_DIR="${STAGE_WORK_DIR}/mnt"
-sudo cp "/usr/bin/qemu-arm-static" "${MNT_DIR}/usr/bin"
+sudo cp /usr/bin/qemu-arm-static "${MNT_DIR}/usr/bin"
 
 log "Clear the preload file"
 sudo cp "${MNT_DIR}/etc/ld.so.preload" "${MNT_DIR}/root"
