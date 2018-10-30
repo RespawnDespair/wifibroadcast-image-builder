f61eefdd012788feb457c5c5fcbb8dd62e59a8e4
Jelle Tigchelaar <RespawnDespair@users.noreply.github.com>
2018-10-30 19:45:54 +0100

    Fixed v5.2.20
    Apparently this is the more stable branche

--- stages/02-Kernel/00-run.sh
+++ stages/02-Kernel/00-run.sh
@@ -8,7 +8,8 @@ log "Download the Raspberry Pi Kernel"
 git clone --depth=1 https://github.com/raspberrypi/linux
 
 log "Download the rtl8812au drivers"
-git clone https://github.com/aircrack-ng/rtl8812au.git
+# Fixed at v5.2.20 as it is deemed more stable for now
+git clone -b v5.2.20 https://github.com/aircrack-ng/rtl8812au.git
 
 #return 
 popd
