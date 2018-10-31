1f1746d672b9d266a9abfa60407aeb8d1aa2c2fb
Jelle Tigchelaar <RespawnDespair@users.noreply.github.com>
2018-10-31 21:53:14 +0100

    doh

--- stages/02-Kernel/03-run.sh
+++ stages/02-Kernel/03-run.sh
@@ -8,7 +8,6 @@ log "Copy Kernel config"
 cp "${STAGE_DIR}/FILES/.config_db_v7_kernel_4_14_66" ./.config
 
 #KERNEL=kernel7 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make bcm2709_defconfig
-KERNEL=kernel7 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make
 KERNEL=kernel7 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make -j4 zImage modules dtbs
 
 log "Saving kernel as ${STAGE_WORK_DIR}/kernel7.img"
