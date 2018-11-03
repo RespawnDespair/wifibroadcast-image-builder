48516bedac2fa1c00b56d042952152dd742296c2
Yes21 <33381245+Yes21@users.noreply.github.com>
2018-11-03 16:12:07 +0100

    4.14.66 kernel source

--- stages/02-Kernel/00-run.sh
+++ stages/02-Kernel/00-run.sh
@@ -5,7 +5,11 @@ log "Remove previous Kernel"
 rm -r linux
 
 log "Download the Raspberry Pi Kernel"
-git clone --depth=1 https://github.com/raspberrypi/linux
+"git clone --depth=1 https://github.com/raspberrypi/linux
+git clone --depth=100 https://github.com/raspberrypi/linux/
+cd linux/
+git checkout c5f7d3c4daf14ba3717fcc1497854d8c365bd742
+cd ..
 
 log "Download the rtl8812au drivers"
 # Fixed at v5.2.20 as it is deemed more stable for now
