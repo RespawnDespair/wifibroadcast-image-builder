f2198056812951d187612bc13e71aeaaab42b095
Jelle Tigchelaar <jtigchelaar@gmail.com>
2018-11-04 14:13:44 +0100

    Sticky kernel version

--- stages/02-Kernel/00-run.sh
+++ stages/02-Kernel/00-run.sh
@@ -1,15 +1,22 @@
+set -e
 # Do this to the WORK folder of this stage
 pushd ${STAGE_WORK_DIR}
 
 log "Remove previous Kernel"
-rm -r linux
+rm -r linux || true
 
 log "Download the Raspberry Pi Kernel"
-git clone --depth=1 https://github.com/raspberrypi/linux
+git clone --depth=100 https://github.com/raspberrypi/linux
+
+pushd linux
 # Switch to specific commit
 git checkout $GIT_KERNEL_SHA1
 
+# out
+popd
+
 log "Download the rtl8812au drivers"
+rm -r rtl8812au || true
 # Fixed at v5.2.20 as it is deemed more stable for now
 git clone https://github.com/aircrack-ng/rtl8812au.git
 
