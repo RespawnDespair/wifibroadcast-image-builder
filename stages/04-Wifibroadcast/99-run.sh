8a32f3ee635f243ea6d5ac6087615a92fb2861e4
Jelle Tigchelaar <jtigchelaar@gmail.com>
2018-12-05 09:20:43 +0100

    Label for boot partition

--- stages/04-Wifibroadcast/99-run.sh
+++ stages/04-Wifibroadcast/99-run.sh
@@ -4,5 +4,11 @@ pushd ${STAGE_WORK_DIR}
 MNT_DIR="${STAGE_WORK_DIR}/mnt"
 sudo cp "$MNT_DIR/root/ld.so.preload" "$MNT_DIR/etc/ld.so.preload"
 
+# Rename the DOS partition
+BOOT_MNT_DIR="${STAGE_WORK_DIR}/mnt/boot"
+BOOT_LOOP_DEV="$(findmnt -nr -o source $BOOT_MNT_DIR)"
+
+fatlabel "$BOOT_LOOP_DEV" "WIFIBRDCAST"
+
 #return
 popd
