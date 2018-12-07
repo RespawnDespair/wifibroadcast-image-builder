8f561e22da4253295ad9845d82a8d478617a6ee2
Jelle Tigchelaar <jtigchelaar@gmail.com>
2018-12-07 17:15:43 +0100

    V4L version 2

--- stages/04-Wifibroadcast/04-run.sh
+++ stages/04-Wifibroadcast/04-run.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 
-echo "
+on_chroot << EOF
+
 # Install OpenVG and flir stuff
 cd /home/pi
 sudo mkdir /home/pi/flir
 sudo mv v4l2loopback /home/pi/flir
 
-echo 'VER ${KERNEL_VERSION}'
-
 cd flir
 cd v4l2loopback
+
+# for V6 architecture
 sudo make KERNELRELEASE=$KERNEL_VERSION
 sudo make KERNELRELEASE=$KERNEL_VERSION install
-
-# COMMENTED OUT AS IT IS CAUSING FATAL ERROR DUE TO WRONG VERSION (uname issue)
 sudo depmod -a -w $KERNEL_VERSION
 
+# for V7 architecture
+sudo make KERNELRELEASE=$KERNEL_VERSION_V7
+sudo make KERNELRELEASE=$KERNEL_VERSION_V7 install
+sudo depmod -a -w $KERNEL_VERSION_V7
+
+# move this to 00 script
 cd /home/pi/flir
 sudo git clone https://github.com/fnoop/flirone-v4l2.git
 cd flirone-v4l2
 sudo make
-" > tmpfile
 
-on_chroot < tmpfile
+EOF
